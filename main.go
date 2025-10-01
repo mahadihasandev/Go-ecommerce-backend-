75b482a9cfa8f12e5e354b959b0fec69f9cbf2fc
mahadihasandev <mayhaydihasan.com@gmail.com>
2025-10-01 08:12:51 +0600

    get request

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func handleAbout(write http.ResponseWriter,read *http.Request){
+	fmt.Fprintln(write,"this is service")
+}
+
+type Product struct{
+	Id int
+	Title string
+	Description string
+	Price float64
+	ImgUrl string
+}
+
+var ProductList [] Product
+
+func handleProduct(write http.ResponseWriter,read *http.Request){
+	write.Header().Set("Access Control Allow Origin","*")
+	write.Header().Set("Content-Type","Application/json")
+
+	if(read.Method!="GET"){
+		http.Error(write,"Send a get request",400)
+		return
+	}
+
+	encoder:=json.NewEncoder(write)
+
+	encoder.Encode(ProductList)
+}
+
+func main(){
+	mux:=http.NewServeMux()
+
+	mux.HandleFunc("/about",handleAbout)
+
+	mux.HandleFunc("/product",handleProduct)
+
+	fmt.Println("server is running in port:8000")
+
+	err:=http.ListenAndServe(":8000",mux)
+	if(err!=nil){
+		fmt.Println(err)
+	}
+}
+
+func init(){
+	prod1:=Product{
+		Id: 1,
+		Title: "big mac",
+		Description: "good quality food for alien",
+		Price: 39.99,
+		ImgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-vqKcsZM2d2WxBPHhMD6IG5xDC-K-TDD59Q&s",
+	}
+
+	prod2:=Product{
+		Id: 2,
+		Title: "Mango",
+		Description: "good quality food for cyborg",
+		Price: 29.99,
+		ImgUrl: "https://www.biovie.fr/img/cms/histoire-origine-mangue.png",
+	}
+
+	prod3:=Product{
+		Id: 3,
+		Title: "Banana",
+		Description: "good quality food for Meta-human",
+		Price: 19.99,
+		ImgUrl: "https://nutritionsource.hsph.harvard.edu/wp-content/uploads/2018/08/bananas-1354785_1920.jpg",
+	}
+
+	ProductList = append(ProductList, prod1)
+	ProductList = append(ProductList, prod2)
+	ProductList = append(ProductList, prod3)
+
+
+}
